ccb7117a2ff97c6d5e07697fc6b1394b0a9f3237
Nguyen Thanh Luc <luc.nguyen150198@gmail.com>
2023-02-22 15:00:07 +0700

    fix folder

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Onboarding from './src/screens/onboarding/Onboarding';
-import Welcome from './src/screens/onboarding/Welcome';
+import Welcome from './src/screens/welcome/Welcome';
 
 AppRegistry.registerComponent(appName, () => Welcome);
